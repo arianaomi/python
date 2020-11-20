eec6c63cf84aed76f4a47ec3ef79794281f63c12
Arianaomi <arianaomi.lp@gmail.com>
2020-11-20 01:43:31 -0600

    ADD delete  methods

--- linked_list.py
+++ linked_list.py
@@ -26,6 +26,7 @@ class LinkedList:
                 print(n.data)
                 n = n.next
     
+    # Insert Methods
     def insert_at_start (self, new_data):
         new_node = Node(new_data)
         new_node.next = self.head
@@ -48,10 +49,34 @@ class LinkedList:
         while (last.next):
             last = last.next
         last.next = new_node
+    
+    # Delete Methods
+    def delete_at_start(self):
+        if self.head is None:
+            print('The list has not element to delete')
+            return
+        self.head = self.head.next
+
+    def delete_node(self,key): # Delete te first node with the key(value)
+        n = self.head
+        if n is not None:
+            if n.data == key:
+                self.head = n.next
+                n = None
+                return
+        while n is not None:
+            if n.data == key:
+                break
+            prev = n
+            n = n.next
+        if n == None:
+            return
+        prev.next = n.next
+        n = None
 
 
 
-if __name__=='__main__':
+if __name__=='__main__': #Check if  this file is being imported from another module -> Executed when invoked directly
     linked_list = LinkedList()
     linked_list.head = Node('Monday') # Create a new node
     second_day = Node('Tuesday')
@@ -59,9 +84,14 @@ if __name__=='__main__':
 
     linked_list.head.next = second_day # Link first node with second
     second_day.next = fourth_day #Link second node with fourth
-
     # insert elements
+
+    linked_list.insert_at_end('Friday')
+    linked_list.insert_at_end('Saturday')
     linked_list.insert_at_start('Sunday') 
     linked_list.insert_after_item(second_day,'Wednesday')
-    linked_list.insert_at_end('Friday')
+    # Detele a node 
+    linked_list.delete_at_start()
+    linked_list.delete_node('Saturday')
     linked_list.print_list()
+
